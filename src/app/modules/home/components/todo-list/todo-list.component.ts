548a2d294e02415ef2471856c6cdba67b0f56af4
Denis Monteiro <deniswmonteiro@gmail.com>
2023-04-27 15:06:40 -0300

    Criação da funcionalidade para deletar itens da lista de tarefas

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interfaces
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [{
+    task: "Nova tarefa",
+    checked: true
+  }];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public deleteItemTaskList(value: number): void {
+    this.taskList.splice(value, 1);
   }
 
+  public deleteAllTaskList(): void {
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+
+    if (confirm) this.taskList = [];
+  }
 }
